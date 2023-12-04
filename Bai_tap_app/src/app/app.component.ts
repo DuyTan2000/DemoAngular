665822f5f0c2e106b948c6d04f712f84bf0503ec
tannd2@imt-soft.com <Tan Nguyen>
2023-12-04 17:49:53 +0700

    Internationalization

--- Bai_tap_app/src/app/app.component.ts
+++ Bai_tap_app/src/app/app.component.ts
@@ -1,4 +1,18 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular 16 Crud example';
+// }
+
+//===================
+
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  constructor(public translateService: TranslateService) {
+  }
+
+  public changeLanguage(language: string): void {
+    this.translateService.use(language);
+  }
+
   title = 'Angular 16 Crud example';
 }
+
--- Bai_tap_app/src/app/app.module.ts
+++ Bai_tap_app/src/app/app.module.ts
@@ -1,13 +1,54 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+// import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+// import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     AddTutorialComponent,
+//     TutorialDetailsComponent,
+//     TutorialsListComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     HttpClientModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+// ==============================================================
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +61,15 @@ import { TutorialsListComponent } from './components/tutorials-list/tutorials-li
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en-US',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
